c166b6889ed21b1efd4e7936a45a434c3538fb07
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-03-23 19:48:23 -0400

    Update GuiMainMenu.java

--- src/main/java/net/minecraft/src/GuiMainMenu.java
+++ src/main/java/net/minecraft/src/GuiMainMenu.java
@@ -86,7 +86,7 @@ public class GuiMainMenu extends GuiScreen {
 				this.splashText = "missingno";
 			}
 		}
-		this.field_92025_p = EaglerAdapter._wisWebGL() ? ("eaglercraft javascript runtime") : ("eaglercraft desktop runtime");
+		this.field_92025_p = EaglerAdapter._wisWebGL() ? ("Minecraft S-SP Online HTML") : ("Minecraft S-SP Local HTML");
 		this.start = System.currentTimeMillis();
 		this.start += this.start % 10000l;
 		this.ackLines = new ArrayList();
